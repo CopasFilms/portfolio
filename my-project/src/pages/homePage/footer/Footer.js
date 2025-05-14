7ac66baf011cd410c570066055f1ea973f7c7c6e
Matheus Felipe <felipe_2012matheus@hotmail.com>
2025-05-14 17:32:47 -0300

    Melhorando o design do footer

--- my-project/src/pages/homePage/footer/Footer.js
+++ my-project/src/pages/homePage/footer/Footer.js
@@ -1,7 +1,13 @@
 import './Footer.css';
 
 import useTexts from '../../../hooks/useTexts';
-import iconLink from '../../../assets/icon/iconLink.svg'
+import Divider from '../../../components/divider/Divider';
+
+import SocialButton from '../../../components/buttons/buttonRedeSocial/SocialButton';
+import linkedinIcon from '../../../assets/icon/SocialLinkedin.svg';
+import tiktokIcon from '../../../assets/icon/SocialTiktok.svg';
+import youtubeinIcon from '../../../assets/icon/SocialYoutube.svg';
+
 
 const Footer = () => {
     const texts = useTexts();
@@ -9,8 +15,22 @@ const Footer = () => {
     return (
         <section id="footer">
             <div className="footer-panel">
-                <p className="footer-textCopas">{texts.TitlePage_Footer}</p>
-                <p className="footer-created">{texts.Created_Footer}<a className="footer-link" href="https://matheusfelipe20.github.io/portfolio/"> MatheusFelipe20 <img className="icon-link-footer" src={iconLink} alt="icone link"/></a></p>
+                <div className="footer-header">
+                    <div>
+                        <p className="footer-textCopas">{texts.TitlePage_Footer}</p>
+                        <p className="footer-subtextCopas">{texts.SubTitlePage_01_Footer}<br />{texts.SubTitlePage_02_Footer}</p>
+                    </div>
+                    <div>
+                        <p className="footer-textCopas">{texts.SocialPage_Footer}</p>
+                            <div className="redeSocial-contact">
+                                <SocialButton height={"28px"} width={"28px"} icon={tiktokIcon} alt="Botão Tiktok" href="" />
+                                <SocialButton height={"28px"} width={"28px"} icon={linkedinIcon} alt="Botão Linkedin" href="" />
+                                <SocialButton height={"28px"} width={"28px"} icon={youtubeinIcon} alt="Botão YouTube" href="" />
+                            </div>
+                    </div>
+                </div>
+                <Divider margin={'20px 0px'} width={'90%'} thickness={'1px'} color={'#A4A4A4'}/>
+                <p className="footer-created">{texts.Created_Footer} <a className="footer-link" href="https://matheusfelipe20.github.io/portfolio/">MatheusFelipe20</a></p>
             </div>
         </section>
     );
