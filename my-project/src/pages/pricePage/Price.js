cb8173a218c4d2b97e52db7f73afe33d26ce3d0d
Matheus Felipe <felipe_2012matheus@hotmail.com>
2025-11-20 16:52:48 -0300

    Mudança na tela de orçamento

--- my-project/src/pages/pricePage/Price.js
+++ my-project/src/pages/pricePage/Price.js
@@ -28,104 +28,105 @@ const Price = () => {
             <div>
                 <p className="titleForm-price">{texts.SubTitle_Price}</p>
             </div>
-
-            <CalculatorPrice/>
-
-            <div className="form-price">
-                <p className="descriptionForm-price">{texts.TitleForm_Price}</p>
-                <p className="textFrom-price">{texts.TextForm_Price}</p>
-                <div className="panelObservation-price">
-                    <p className="textObservation-price">{texts.TextObservation01_Price}</p>
-                    <p className="textObservation-price">{texts.TextObservation02_Price}</p>
-                    <p className="textObservation-price">{texts.TextObservation03_Price}</p>
-                </div>
-                <div className="panelComplexity-price">
-                    <div className="cardComplexity-price">
-                        <div className="cardComplexity-header-price">
-                            <div>
-                                <p className="titleComplexity-price">{texts.TitleComplexity_Basic_Price}</p>
-                                <p className="subtitleComplexity-price">{texts.NivelComplexity_Basic_Price}</p>
-                            </div>
-                            <div>
-                                <img src={IconNivel1} className="iconComplexity-price" alt="icone rosto feliz"/>
-                            </div>
-                        </div>
-                        <Divider margin={'0px'} width={'100%'} color={'rgb(226, 228, 233)'} thickness={'1px'}/>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity01_Basic_Price}</p>
-                        </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity02_Basic_Price}</p>
-                        </div>
-                                                <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity03_Basic_Price}</p>
-                        </div>
+            <div className="panel-table-price">
+                <CalculatorPrice/>
+                <div className="form-price">
+                    <p className="descriptionForm-price">{texts.TitleForm_Price}</p>
+                    <p className="textFrom-price">{texts.TextForm_Price}</p>
+                    <div className="panelObservation-price">
+                        <p className="textObservation-price">{texts.TextObservation01_Price}</p>
+                        <p className="textObservation-price">{texts.TextObservation02_Price}</p>
+                        <p className="textObservation-price">{texts.TextObservation03_Price}</p>
                     </div>
-                    <div className="cardComplexity-price">
-                        <div className="cardComplexity-header-price">
-                            <div>
-                                <p className="titleComplexity-price">{texts.TitleComplexity_Intermediary_Price}</p>
-                                <p className="subtitleComplexity-price">{texts.NivelComplexity_Intermediary_Price}</p>
+                    <div className="panelComplexity-price">
+                        <div className="cardComplexity-price">
+                            <div className="cardComplexity-header-price">
+                                <div>
+                                    <p className="titleComplexity-price">{texts.TitleComplexity_Basic_Price}</p>
+                                    <p className="subtitleComplexity-price">{texts.NivelComplexity_Basic_Price}</p>
+                                </div>
+                                <div>
+                                    <img src={IconNivel1} className="iconComplexity-price" alt="icone pincel"/>
+                                </div>
                             </div>
-                            <div>
-                                <img src={IconNivel2} className="iconComplexity-price" alt="icone foguete"/>
+                            <Divider margin={'0px'} width={'100%'} color={'rgb(226, 228, 233)'} thickness={'1px'}/>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity01_Basic_Price}</p>
                             </div>
-                        </div>
-                        <Divider margin={'0px'} width={'100%'} color={'rgb(226, 228, 233)'} thickness={'1px'}/>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                                <p className="textComplexity-price">{texts.TextComplexity01_Intermediary_Price}</p>
-                        </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity02_Intermediary_Price}</p>
-                        </div>
-                                                <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity03_Intermediary_Price}</p>
-                        </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity04_Intermediary_Price}</p>
-                        </div>
-                    </div>
-                    <div className="cardComplexity-price">
-                        <div className="cardComplexity-header-price">
-                            <div>
-                                <p className="titleComplexity-price">{texts.TitleComplexity_Professional_Price}</p>
-                                <p className="subtitleComplexity-price">{texts.NivelComplexity_Professional_Price}</p>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity02_Basic_Price}</p>
                             </div>
-                            <div>
-                                <img src={IconNivel3} className="iconComplexity-price" alt="icone pessoa"/>
+                                                    <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity03_Basic_Price}</p>
                             </div>
                         </div>
-                        <Divider margin={'0px'} width={'100%'} color={'rgb(226, 228, 233)'} thickness={'1px'}/>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity01_Professional_Price}</p>
+                        <div className="cardComplexity-price">
+                            <div className="cardComplexity-header-price">
+                                <div>
+                                    <p className="titleComplexity-price">{texts.TitleComplexity_Intermediary_Price}</p>
+                                    <p className="subtitleComplexity-price">{texts.NivelComplexity_Intermediary_Price}</p>
+                                </div>
+                                <div>
+                                    <img src={IconNivel2} className="iconComplexity-price" alt="icone foguete"/>
+                                </div>
+                            </div>
+                            <Divider margin={'0px'} width={'100%'} color={'rgb(226, 228, 233)'} thickness={'1px'}/>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                    <p className="textComplexity-price">{texts.TextComplexity01_Intermediary_Price}</p>
+                            </div>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity02_Intermediary_Price}</p>
+                            </div>
+                                                    <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity03_Intermediary_Price}</p>
+                            </div>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity04_Intermediary_Price}</p>
+                            </div>
                         </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity02_Professional_Price}</p>
-                        </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity03_Professional_Price}</p>
-                        </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity04_Professional_Price}</p>
-                        </div>
-                        <div className="content-textComplexity-price">
-                            <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
-                            <p className="textComplexity-price">{texts.TextComplexity05_Professional_Price}</p>
+                        <div className="cardComplexity-price">
+                            <div className="cardComplexity-header-price">
+                                <div>
+                                    <p className="titleComplexity-price">{texts.TitleComplexity_Professional_Price}</p>
+                                    <p className="subtitleComplexity-price">{texts.NivelComplexity_Professional_Price}</p>
+                                </div>
+                                <div>
+                                    <img src={IconNivel3} className="iconComplexity-price" alt="icone camera"/>
+                                </div>
+                            </div>
+                            <Divider margin={'0px'} width={'100%'} color={'rgb(226, 228, 233)'} thickness={'1px'}/>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity01_Professional_Price}</p>
+                            </div>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity02_Professional_Price}</p>
+                            </div>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity03_Professional_Price}</p>
+                            </div>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity04_Professional_Price}</p>
+                            </div>
+                            <div className="content-textComplexity-price">
+                                <img src={IconCorrect} className="iconTextComplexity-price" alt="icone correto"/>
+                                <p className="textComplexity-price">{texts.TextComplexity05_Professional_Price}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
         </div>
         <Footer/>
         </>
